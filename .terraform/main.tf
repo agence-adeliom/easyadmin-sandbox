695915272732bb16f011de5ffec391d0872d215d
Arnaud RITTI <arnaud.ritti@adeliom.com>
2022-08-25 16:53:12 +0200

    no message

--- .terraform/main.tf
+++ .terraform/main.tf
@@ -25,9 +25,8 @@ data "github_repository" "repository" {
   name = var.repo
 }
 
-
 resource "github_repository" "create_repository" {
-  count = data.github_repository.repository.repo_id != null ? 1 : 0
+  count = data.github_repository.repository.repo_id == null ? 1 : 0
   name = var.repo
   visibility = "public"
 
