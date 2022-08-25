ce4ab9e0328b554ac6340ed0a489a92bc9a5be5d
Arnaud RITTI <arnaud.ritti@adeliom.com>
2022-08-25 16:49:27 +0200

    tf test

--- .terraform/main.tf
+++ .terraform/main.tf
@@ -21,9 +21,14 @@ provider "github" {
   owner = "agence-adeliom"
 }
 
+data "github_repository" "repository" {
+  name = var.repo
+}
 
-resource "github_repository" "repository" {
-  name        = var.repo
+
+resource "github_repository" "create_repository" {
+  count = data.github_repository.repository.repo_id != null ? 1 : 0
+  name = var.repo
   visibility = "public"
 
   has_issues = true
