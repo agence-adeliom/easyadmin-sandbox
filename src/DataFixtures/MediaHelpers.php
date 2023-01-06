79cf2bbc50f09393fb25e5c72601b547523d2baa
Arnaud RITTI <arnaud.ritti@adeliom.com>
2023-01-06 15:28:34 +0100

    fix: fix issues
    easy-media-bundle:
    	fix #17 - Exception in MediaSubscriber
    	fix #15 - Error in fileIsType method
    
    easy-admin-user-bundle:
    	fix #1 - Translation error - messages.fr.yaml
    
    easy-page-bundle:
    	fix - Missing attributes for forward request

---
+++ src/DataFixtures/MediaHelpers.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ *  * This file has been edited by Adeliom.
+ *  * Adeliom team <contact@adeliom.com>
+ */
+
+namespace App\DataFixtures;
+
+use Adeliom\EasyMediaBundle\Exception\AlreadyExist;
+use League\Flysystem\FilesystemException;
+use League\Flysystem\UnableToCopyFile;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\String\Slugger\AsciiSlugger;
+
+trait MediaHelpers
+{
+    public function createMedia($folderPath = 'pages/home', $fileName = 'cta-market-col1.jpeg')
+    {
+        $projectDir = $this->kernel->getProjectDir();
+        $manager = $this->easyMediaManager;
+
+        try {
+            return $manager->createMedia(
+                sprintf('%s/var/fixtures_medias/%s/%s', $projectDir, $folderPath, $fileName),
+                $folderPath,
+                $fileName
+            );
+        } catch (AlreadyExist|FileException|UnableToCopyFile) {}
+
+        return null;
+    }
+}
