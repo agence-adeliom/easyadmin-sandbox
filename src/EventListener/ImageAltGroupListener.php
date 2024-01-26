01a5da4a03846cb09e42a5c9c544df67ff77c54b
JeanDavidDaviet <jeandaviddaviet@gmail.com>
2024-01-26 14:48:08 +0100

    allow to generate alt text if enable_generating_alts config is enabled

---
+++ src/EventListener/ImageAltGroupListener.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\EventListener;
+
+use Adeliom\EasyMediaBundle\Event\EasyMediaGenerateAllAlt;
+use Adeliom\EasyMediaBundle\Service\EasyMediaManager;
+use App\Message\EasyMediaGenerateAltMessage;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+#[AsEventListener(EasyMediaGenerateAllAlt::NAME)]
+class ImageAllAltListener
+{
+    public function __construct(
+        private EasyMediaManager $easyMediaManager,
+        private MessageBusInterface $messageBus,
+    ) {
+    }
+
+    public function __invoke(EasyMediaGenerateAllAlt $event): void
+    {
+        $medias = $this->easyMediaManager->getHelper()->getMediaRepository()->findAll();
+        if (!empty($medias)) {
+            foreach ($medias as $media) {
+                $this->messageBus->dispatch(new EasyMediaGenerateAltMessage($media->getId()));
+            }
+        }
+    }
+}
